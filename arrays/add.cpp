0fcd8517db89eb2c39df3a00e6fd90db7b941b9c
LakshyaVerma123kl <lakshya123kl@gmail.com>
2025-08-07 13:00:06 +0530

    new

---
+++ arrays/add.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Ask for input
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Output result
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
